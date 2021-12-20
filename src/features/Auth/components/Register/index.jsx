6b4ac964671eb97f9ed0fce066ea54defec2440f
Shizuku <nevergivup282000@gmail.com>
2021-12-20 23:25:18 +0700

    api register user

--- src/features/Auth/components/Register/index.jsx
+++ src/features/Auth/components/Register/index.jsx
@@ -1,16 +1,30 @@
+import { unwrapResult } from "@reduxjs/toolkit";
+import {register} from "features/Auth/userSlice";
 import React from "react";
+import { useDispatch } from "react-redux";
 import RegisterForm from "../RegisterForm";
 
 Register.propTypes = {};
 
 function Register(props) {
-  const handleSubmit = (values) => {
-    console.log("handle submit register form!", values);
+  const dispatch = useDispatch();
+  const handleSubmit = async (values) => {
+    try {
+      // auto set username = email (follow api)
+      values.username = values.email;
+
+      const action = register(values);
+      const resultAction = await dispatch(action);
+      const user = unwrapResult(resultAction);
+      console.log("🚀 new user", user);
+    } catch (error) {
+      console.log("🚀 error handleSubmit in RegisterForm:", error);
+    }
   };
 
   return (
     <div>
-      <RegisterForm onSubmit={handleSubmit}/>
+      <RegisterForm onSubmit={handleSubmit} />
     </div>
   );
 }
--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,33 +1,36 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import userApi from "api/userApi";
 
-// First, create the thunk
-const register = createAsyncThunk(
-  'user/register',
-  async (payload) => {
-    // call api to register
+// First, create the thunk. Bản chất bên dưới sd redux thunk middleware
+// return resolved promise
+// RTK export unwrapResult func that can be used to extract payload or error form action
+// and return or throw the result
+export const register = createAsyncThunk("user/register", async (payload) => {
+  // call api to register
+  const data = await userApi.register(payload);
 
-    // save data to local storage
+  // save data to local storage
+  localStorage.setItem("access_token", data.jwt);
+  localStorage.setItem("user", JSON.stringify(data.user));
 
-    return {}
-  }
-)
+  // return user data (day ra payload ben trong extractReducer)
+  return data.user;
+});
+
 const userSlice = createSlice({
   name: "user",
   initialState: {
     current: {},
     settings: {},
   },
-  reducers: {
-    
-  },
+  reducers: {},
   // update
-  extractReducers: {
+  extraReducers: {
     // 'user/register/fulfilled': () => {},
     [register.fulfilled]: (state, action) => {
-      
+      state.current = action.payload;
     },
-  }
+  },
 });
 
 const { reducer } = userSlice;
